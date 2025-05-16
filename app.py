4e2e727e9bc09e9f974a12e6cda59b636623d0e0
aryanp.031206@gmail.com <aryanpatel31206@gmail.com>
2025-05-16 18:36:35 -0400

    added option to paste text

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ load_dotenv()  # load the environment variables
 
 app = Flask(__name__)
 
+# intial setup for sessions
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -27,7 +28,8 @@ def upload():
         return render_template("upload.html")
 
     # POST
-    file = request.files.get("file")
+    file = request.files.get("fileInput")
+    text = request.form.get("textInput")
     text_content = ""
 
     # only proceed if the uploaded file is a pdf
@@ -39,25 +41,29 @@ def upload():
             if page_text:
                 text_content += page_text
 
-        # convert text to audio
-        audio = gTTS(text_content, lang="en")
-        audio.save("static/audio.mp3")
-        
-        # generate summary using gemini
-        client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+    # process the text if the user pasted the text
+    elif text:
+        text_content = text
 
-        response = client.models.generate_content(
-            model="gemini-2.0-flash",
-            contents=[text_content, os.getenv("PROMPT")],
-        )
-
-        # store the response in a session and redirect the user to result page
-        session["summary"] = response.text
-        return redirect("/result")
-         
     else:
         return redirect("/error")
+    
+    # convert text to audio
+    audio = gTTS(text_content, lang="en")
+    audio.save("static/audio.mp3")
+    
+    # generate summary using gemini
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=[text_content, os.getenv("PROMPT")],
+    )
+
+    # store the response in a session and redirect the user to result page
+    session["summary"] = response.text
+    return redirect("/result")
+    
 
 @app.route("/result")
 def result():
